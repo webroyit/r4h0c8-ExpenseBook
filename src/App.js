cd0e19bf9b7c5cfd2af98e8941675238943010da
royweb <xytank@gmail.com>
2020-04-09 12:18:13 -0400

    Add balance component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Balance } from './components/Balance.js';
 
 import './App.css';
 
@@ -7,6 +8,9 @@ function App() {
   return (
     <>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Header = () => {
     return(
-        <h1>
+        <h2>
             Expense Book
-        </h1>
+        </h2>
     );
 };
