318b092ec0b64b68717f10da46cf716faf376a48
royweb <xytank@gmail.com>
2020-04-09 12:39:20 -0400

    Add a component for income and expense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance.js';
+import { IncomeExpenses } from './components/IncomeExpenses.js';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
       </div>
     </>
   );
