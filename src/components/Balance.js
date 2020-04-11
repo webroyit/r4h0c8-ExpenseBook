0512e56eb0991de8a50d2f5f9f23718078acada1
royweb <xytank@gmail.com>
2020-04-11 14:17:12 -0400

    Fix naming problem and display the total

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
     const { transactions } = useContext(GlobalContext);
 
     // to get the total amount on the transactions
-    const amounts = transactions.map(transcation => transactions.amount);
+    const amounts = transactions.map(transaction => transaction.amount);
     const total = amounts.reduce((total, item) => (total += item), 0).toFixed(2);
 
     return(
         <>
             <h4>Your Balance</h4>
-            <h1 id="balance">$0.00</h1>
+            <h1 id="balance">${total}</h1>
         </>
     );
 };
