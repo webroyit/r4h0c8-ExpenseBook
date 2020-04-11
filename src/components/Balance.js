50d2472500ba7b6345ab2ba41e6d0089e6623172
royweb <xytank@gmail.com>
2020-04-11 14:03:18 -0400

    Add functions to get the total amount on the transactions

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    // to get the total amount on the transactions
+    const amounts = transactions.map(transcation => transactions.amount);
+    const total = amounts.reduce((total, item) => (total += item), 0).toFixed(2);
+
     return(
         <>
             <h4>Your Balance</h4>
